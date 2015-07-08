b0fe9856bf0c4a42dc586794c2e9a5e4f25c9fe2
ismail-codar <icodar@hotmail.com>
2015-07-08 03:50:59 +0300

    bin

--- commandline.js
+++ commandline.js
@@ -6,8 +6,14 @@ var convert = require('./');
 var fileName = process.argv[2];
 
 if (!fileName) {
-    throw new Error('supply input HTML file as first argument');
+    throw new Error('HTML file must be first argument');
 }
 
-process.stdout.write(convert(fs.readFileSync(fileName)));
-process.stdout.write("\n");
+var output = convert(fs.readFileSync(fileName, "utf8").toString());
+
+if(process.argv.length == 3) {
+    console.log(output);
+} else {
+    fs.writeFileSync(process.argv[3], output, "utf8");
+    console.log("<<" + process.argv[3] + ">>");
+}
